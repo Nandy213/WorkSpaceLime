74abaf1e1e696fe70b474e7dc2b8ffe420dcdbbd
Nandy213 <nandhini285.munirathinam@gmail.com>
2023-06-18 19:58:27 +0530

    newLime

---
+++ src/test/java/Com/LimeRoad/AppTest.java
@@ -0,0 +1,27 @@
+package Com.LimeRoad;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("First demo");
+        
+    }
+     public static void main(String [] agrs) {
+    	 AppTest t=new AppTest();
+    	 t.shouldAnswerWithTrue();
+     }
+
+
+}
